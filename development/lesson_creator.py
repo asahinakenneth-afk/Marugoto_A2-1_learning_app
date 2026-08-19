91972f666f67eb942226dba9ee4772752a28c89d
asahinakenneth-afk <asahinakenneth@gmail.com>
2026-08-19 15:33:49 -0400

    EDIT: completely working vocabulary phase ADD: particles and adjectives as separate structures alongside verbs (due to conjugations)

--- development/lesson_creator.py
+++ development/lesson_creator.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import json
 
 '''  Similar to glossary_insert.py, I made this to help people insert the lessons in the program!
-    I wish it had an interface... but anyways, to start it's very good
+    I wish it had an interface... but anyways, to start it's very good... I think?
 '''
 
 BASE_DIR = Path(__file__).resolve().parent ## just to make sure...
@@ -11,7 +11,7 @@ run = True
 while run:
 
     print("Make sure you aren't trying to add a pre-existing lesson" \
-          "or all the information will be lost!")
+          "or all the information WILL be lost (rewritten)!")
     new_lesson = input("Do you want to add a new lesson? (Y/N)").lower()
 
     while new_lesson == "y":
@@ -33,6 +33,8 @@ while run:
         "words" : {
         },
         "verbs" : {
+        },
+        "adjectives" : {
         }
         }''')
         lesson.touch(exist_ok=True)
@@ -42,7 +44,7 @@ while run:
 
         print(f"""Please have your words ready :3!
         1. In the case of verbs, you only need to refer to their number ID in glossary.json
-        same with counters!
+        same with adjectives!
         2. You need to type the word type this way: 
         - Noun  --> n
         - Verb  --> v
@@ -64,12 +66,19 @@ while run:
             type = input("Which word type?: ").lower()
 
             if type == "v":
-                id = str(input("Insert the ID of the verb"))
-                key = len(vocabulary_file["verbs"]) + 1
-                vocabulary_file["verbs"][f"v_{key}"] = id
+                id = str(input("Insert the ID of the verb: "))
+                vocabulary_file["verbs"][f"v_{id}"] = id
+
                 with open(vocabulary, 'w', encoding='utf-8') as archivo:
                     json.dump(vocabulary_file, archivo, ensure_ascii=False, indent=4)
-            else:
+            elif type == "adj":
+                id = str(input("Insert the ID of the adjective: "))
+                vocabulary_file["adjectives"][f"adj_{id}"] = id
+
+                with open(vocabulary, 'w', encoding='utf-8') as archivo:
+                    json.dump(vocabulary_file, archivo, ensure_ascii=False, indent=4)   
+
+            elif type == "n" or type == "adv" or type == "s" or type == "p" :
                 kanji = input("Insert the kanji version of the word: ")
                 kana = input("Insert the kana reading of the word: ")
                 key = len(vocabulary_file["words"]) + 1
@@ -80,9 +89,16 @@ while run:
                 }
                 with open(vocabulary, 'w', encoding='utf-8') as archivo:
                     json.dump(vocabulary_file, archivo, ensure_ascii=False, indent=4)
+            else:
+                print(f'''There has been an unexpected error, try to
+                - Determine if you've wrote the type properly (as said above)''')
 
             repeat = input("Do you wish to add another word? (y/n)").lower()
             if repeat == "y":
                 vocabulary_phase = True
             else: 
                 vocabulary_phase = False
+
+            ''' LESSON PHASE '''
+
+            '''  TEST PHASE  '''
